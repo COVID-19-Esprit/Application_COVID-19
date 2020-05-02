d72684d9549f6db6e6afbe69e99c6fff0f0275df
Achref HAMMAMI <achref.hammami@esprit.tn>
2020-05-02 03:12:41 +0100

    CRUD ADMIN

--- src/application_covid/Main/Application_COVID19.java
+++ src/application_covid/Main/Application_COVID19.java
@@ -5,6 +5,10 @@
  */
 package application_covid.Main;
 
+import Inscription.InterfaceAdmin;
+import Malade.Utilisateur_Malade;
+import Myconnection.myconnection;
+
 /**
  *
  * @author Achref
@@ -16,6 +20,14 @@ public class Application_COVID19 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+           myconnection mc = new myconnection();
+           InterfaceAdmin ia = new InterfaceAdmin();
+           
+         Utilisateur_Malade um = new Utilisateur_Malade(1,"ee22e","yy22yyy",100,"u22uuuu",23601135,"oo22oo","1211");
+       // ia.ajouterMalade(um);
+         ia.update(um,1);
+     //   ia.supprimer(113);
     }
     
 }
